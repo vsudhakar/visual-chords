2659e293479aca7915b10f9cc22b4066482ca046
Alex <adex454@gmail.com>
2015-01-29 08:07:12 -0500

    added colors bindings

--- locals.py
+++ locals.py
@@ -1,5 +1,5 @@
 #constants
-SCREEN_SIZE=(1280,720)
+SCREEN_SIZE=(1280,1024)
 KEYS={
     "K_q":0,
     "K_2":1,
@@ -22,3 +22,18 @@ KEYS={
     "K_EQUALS":18,
     "K_RIGHTBRACKET":19
 }
+
+COLORS={
+    
+    0:(255, 0, 0),
+    1:(255, 127, 0),
+    2:(RGB: 255, 255, 0) (60 Degrees)
+    3:127, 255, 0) (90 Degrees)
+    3:(RGB: 0, 255, 0) (120 Degrees)
+    4:(0, 255, 127) (150 Degrees)
+    5:(0, 255, 255) (180 Degrees)
+    6:(0, 127, 255) (210 Degrees)
+    7:( 0, 0, 255) (240 Degrees)
+    8:(RGB: 127, 0, 255) (270 Degrees)
+    9: 255, 0, 255) (300 Degrees)
+    10:(RGB: 255, 0, 127) (330 Degrees)
