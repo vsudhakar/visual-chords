63892593845cb2cfd962cd86c9ac5a7f3c40d4ea
Alex <adex454@gmail.com>
2015-01-30 08:08:38 -0500

    not enought time

--- chordsmain.py
+++ chordsmain.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from locals.py import *
 #import keyboard, scale, visual, chordbox, chordrec, sound, midicontrol, goals
 #from keyboard import Keyboard
 #from scale import Scale
@@ -11,7 +12,7 @@ from pygame.locals import *
 
 
 pygame.init()
-screen=pygame.display.set
+screen=pygame.display.set_mode(SCREEN_SIZE, pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE)
 
 keyboard=Keyboard()
 scale=Scale()
@@ -25,10 +26,12 @@ clock=pygame.time.Clock()
 #initialisations
 keyboard=Keyboard()
 scale=Scale()
+goals=Goals()
 visual=Visualisation()
 chordbox=Chordbox()
 chordrec=Chordrec()
 sound=Sound()
+
 
 
 
@@ -41,8 +44,26 @@ while True:
             keydown.append(KEYS[event.key])
         elif event.type==KEYUP:
             keyup.append(KEYS[event.key])
-    
+
+    #Run Modules
     keyboard.update(keydown, keyup)
     scale.update(keydown, keyup)
-    screen.blit(scale.surf)
-    screen.blit(keyboard.surf)
+    #goals.update(???)
+    chordrec.update(keydown, keyup)
+    visual.update(keydown, keyup, chordrec.data)
+    chordbox.update(chordrec.chordstring)
+    sound.update(keydown, keyup)
+
+    #Draw Surfaces
+    screen.blit(keyboard.surf, (0,HEIGHT*.2))
+    screen.blit(scale.surf, (WIDTH*.75,HEIGHT*.25))
+    screen.blit(goals.surf, (WIDTH*.75,0))
+    screen.blit(visual.surf, (0, HEIGHT*.10))
+    screen.blit(chordbox.surf, (WIDTH*.1875,0))
+    
+    
+
+    
+    pygame.display.flip()
+
+    
