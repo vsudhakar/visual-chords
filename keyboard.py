ea2d11a5a9548ea458536d758b15ecb2392e4a76
solanthor <ilornil@gmail.com>
2015-02-03 07:32:30 -0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,7 +2,7 @@ from pygame.locals import *
 
 class Keyboard:
     surf = new Surface(SCREEN_SIZE)
-    def update(keyup, keydown):
+    def update(keydown, keyup):
         keyList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
         whiteList = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24] #15
         blackList = [1, 3, 6, 8, 10, 13, 15, 18, 20, 22] #10
