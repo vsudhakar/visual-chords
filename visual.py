518e8aa82dd9480f65d58d8d96edbfba0083f14a
Visrut Sudhakar <visrut@gmail.com>
2015-02-03 08:04:45 -0500

    First iteration of visualization w/ keydown

--- visual.py
+++ visual.py
@@ -2,52 +2,74 @@
 
 import pygame
 from pygame.locals import *
-from locals.py import *
+from locals import *
 
 class visual:
     def __init__(self, size):
-        self.size = (WIDTH*0.75, HEIGHT*0.7) 
+##        self.size = (WIDTH*0.75, HEIGHT*0.7)
+        self.size = size
         self.surf = pygame.Surface(self.size)
 
         #Rectangle visualization
         self.rects = []
         numOfRects = 5
         heightOfRects = int(self.size[1]/5)
-        for i in range(numOfRects):
-            self.rects.append(self.Rect((0, heightOfRects*(i+1)), (self.size[0], heightOfRects))
 
-        #Draw rectangles onto surface object
-        for i in self.rects:
-            pygame.draw.rect(self.surf, pygame.Color("red"), i))
+        self.currentNotes = []
 
-
-
-    def draw(notes):
+    def draw(self, notes):
         for x in xrange(len(notes)):
             h=(x+1)*(self.size[1]/5)
-            r=Pygame.Rect((0,h), self.size[0], h)
+            r=pygame.Rect((0,h-self.size[1]/5), (self.size[0], self.size[1]/5))
             self.surf.fill(COLOR(notes[x]), r)
+        self.currentNotes = notes
             
-    def update(keydown, keyup, chordrec):
+    def update(self, keydown, keyup, chordrec):
         if chordrec==None:
-            
-                              
+            notes = self.currentNotes
+            for i in keydown:
+                notes.pop()
+                notes.insert(0, i)
+            print notes
+            self.draw(keydown)
+            self.currentNotes = notes
 
 
 
 
 
 ##DEBUGGING
+pygame.init()
+size = (512, 510)
+screen=pygame.display.set_mode(size)
+vis = visual(size)
 
-##pygame.init()
-##screen=pygame.display.set_mode((100, 100))
-##
-##vis = visual((100, 100))
-##
-##FPS = 60
-##clock=pygame.time.Clock()
-##
-##screen.blit(vis.surf)
+FPS = 60
+clock=pygame.time.Clock()
+n = [0, 1, 2, 3, 4]
+vis.draw(n)s
+screen.blit(vis.surf, (0, 0))
+
+pygame.display.flip()
+
+
+while True:
+    clock.tick(60)
+    keydown = []
+    keyup = []
+    for event in pygame.event.get():
+            if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if event.type==KEYDOWN:
+                print pygame.key.name(event.key)
+                keydown.append(KEYS["K_"+str(pygame.key.name(event.key))])
+            elif event.type==KEYUP:
+                keyup.append(KEYS["K_"+str(pygame.key.name(event.key))])
+    vis.update(keydown, keyup, None)
+    vis.draw(n)
+    screen.blit(vis.surf, (0, 0))
+    pygame.display.flip()
 
                             
 
