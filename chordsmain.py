c7d7dc87ae8a554b80deb287b13fb023521a2cfc
Alex <adex454@gmail.com>
2015-03-05 23:40:14 -0500

    draws visual boxes and debugs/runs visual, chordbox, and keyboard modules

--- chordsmain.py
+++ chordsmain.py
@@ -8,10 +8,10 @@ from visual import Visual
 from chordbox import Chordbox
 '''
 #import keyboard, scale, visual, chordbox, chordrec, sound, midicontrol, goals
-#from keyboard import Keyboard
+from alexkeyboard import Keyboard
 #from scale import Scale
-#from visual import visual
-
+from visual import visual
+from chordbox import Chordbox
 
 
 
@@ -31,11 +31,11 @@ clock=pygame.time.Clock()
 
 
 #initialisations
-##keyboard=Keyboard()
+keyboard=Keyboard()
 ##scale=Scale()
 ##goals=Goals()
-##visual=visual()
-##chordbox=Chordbox()
+visual=visual()
+chordbox=Chordbox()
 ##chordrec=Chordrec()
 ##sound=Sound()
 
@@ -45,7 +45,7 @@ pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,HEIGHT*.8, WIDTH*.75, HEIGHT*.2)
 pygame.draw.rect(screen, (0,0,0), pygame.Rect(WIDTH*.75,HEIGHT*.75, WIDTH*.25, HEIGHT*.25), 2)
 pygame.draw.rect(screen, (0,0,0), pygame.Rect(WIDTH*.75,0, WIDTH*.25, HEIGHT*.75), 2)
 pygame.draw.rect(screen, (0,0,0), pygame.Rect(0,HEIGHT*.1, WIDTH*.75, HEIGHT*.7), 2)
-pygame.draw.rect(screen, (0,0,0), pygame.Rect(WIDTH*.15,0, WIDTH*.5, HEIGHT*.1), 2)
+pygame.draw.rect(screen, (0,0,0), pygame.Rect(WIDTH*.125,0, WIDTH*.5, HEIGHT*.1), 2)
 
 while True:
     keydown=[]
@@ -61,21 +61,21 @@ while True:
                 keyup.append(KEYS[event.key])
             except:
                 pass
-            ##    keyboard.update(keydown, keyup)
+        keyboard.update(keydown, keyup)
             ##    scale.update(keydown, keyup)
             ##    chordrec.update(keydown, keyup)
             ##    goals.update(keydown, keyup, chordrec.data)
-            ##    visual.update(keydown, keyup, None)        # For now 'None' is passed, it should be chordrec.data
+        visual.update(keydown, keyup, None)        # For now 'None' is passed, it should be chordrec.data
 
-            ##    chordbox.update(chordrec.chordstring)
+        chordbox.update("thing", "lots of more text to render !@#$^%&*()")
             ##    sound.update(keydown, keyup)
 
                 #Draw Surfaces
-            ##    screen.blit(keyboard.surf, (0,HEIGHT*.8))
+        screen.blit(keyboard.surf, (0,HEIGHT*.8))
             ##    screen.blit(scale.surf, (WIDTH*.75,HEIGHT*.25))
             ##    screen.blit(goals.surf, (WIDTH*.75,0))
-             ##   screen.blit(visual.surf, (0, HEIGHT*.10))
-            ##    screen.blit(chordbox.surf, (WIDTH*.1875,0))
+        screen.blit(visual.surf, (0, HEIGHT*.10))
+        screen.blit(chordbox.surf, (WIDTH*.125,0))
             
 
 
