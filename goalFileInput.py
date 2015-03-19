cbd9b21c2c808063106b9cbaeec9ebc49a6e41a5
vsudhakar <visrut@suvithra.com>
2015-03-19 00:54:23 -0400

    Added interval support for goals

--- goalFileInput.py
+++ goalFileInput.py
@@ -16,15 +16,17 @@ while index + 1 < len(content):
     goalQualifier = content[index+1]
     if str(goalText)[0] != '#':
         if 'major third' in str(goalQualifier).lower():
-            goals.append((str(goalText), (4, None, None, None)))
+            goals.append((str(goalText), (None, 4)))
         elif 'minor third' in str(goalQualifier).lower():
-            goals.append((str(goalText), (3, None, None, None)))
+            goals.append((str(goalText), (None, 3)))
         elif 'major triad' in str(goalQualifier).lower():
             goals.append((str(goalText), (None, 0, 0, None)))
         elif 'f# minor diminished triad' in str(goalQualifier).lower():
             goals.append((str(goalText), (6, 2, 0, None)))
         elif 'minor triad' in str(goalQualifier).lower():
             goals.append((str(goalText), (None, 1, 0, None)))
+        elif 'perfect fifth' in str(goalQualifier).lower():
+            goals.append((str(goalText), (None, 7)))
         else:
             goals.append((str(goalText), (None, None, None, None)))
     index += 2
